dae496c4d75bb0b8c60f62fd0310bb2aead842f2
Anthony Sottile <asottile@umich.edu>
2017-12-28 20:20:10 -0800

    v1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='reorder_python_imports',
     description='Tool for reordering python imports',
     url='https://github.com/asottile/reorder_python_imports',
-    version='0.3.5',
+    version='1.0.0',
     author='Anthony Sottile',
     author_email='asottile@umich.edu',
     classifiers=[
